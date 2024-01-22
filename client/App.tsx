b80474921aac1f6aa41264054e8057a2a7a15965
Marcos Lambir Torres <markos_laviana@yahoo.es>
2024-01-22 11:41:23 +0100

    feat: adding Hello component

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Hello from "./src/components/hello/Hello.tsx";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Hello name="Marcos" />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
